43bb6a5795cae0a3e8848bc1db289df9c9413b5e
yrkFukushima <82563469+yrkFukushima@users.noreply.github.com>
2021-05-24 18:22:02 +0900

    ログイン機能の修正

--- db/schema.rb
+++ db/schema.rb
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_05_24_032104) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
