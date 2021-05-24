c88fddf434fca23a40e864311bd4172cd95e1aa8
yrkFukushima <82563469+yrkFukushima@users.noreply.github.com>
2021-05-24 17:07:55 +0900

    保存機能の実装

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,5 +1,5 @@
 class Dish < ApplicationRecord
   belongs_to :user
 
-  validates :dish_1, presence: ture
+  validates :dish_1, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "dishes#index"
-  resources :dishes, only: [:create]
+  resources :dishes, only: [:new, :show, :create]
 end
