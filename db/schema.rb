c944563cc69a95bcfc3978cdb99cf9ba86d34f53
yrkFukushima <82563469+yrkFukushima@users.noreply.github.com>
2021-05-24 12:28:31 +0900

    DBの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_24_023128) do
+ActiveRecord::Schema.define(version: 2021_05_24_032104) do
+
+  create_table "dishes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "dish_1", null: false
+    t.string "dish_2"
+    t.string "dish_3"
+    t.string "dish_4"
+    t.string "dish_5"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_dishes_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,4 +36,5 @@ ActiveRecord::Schema.define(version: 2021_05_24_023128) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "dishes", "users"
 end
